760022a473e07199c6b24aafe9b1f3402feb8bb7
Chirs <874552640@qq.com>
2024-02-24 15:17:12 +0800

    重构第二个函数

--- sketch.js
+++ sketch.js
@@ -12,18 +12,60 @@ function setup() {
     createCanvas(1000,1000);
     img.resize(120,160);
     //load image pixel values into array pixels
-
+    image(img,0,0);
 
     grayscale(img);
-    // image(greyscale_img,img.width,0);
+
+    RGB_channels(img);
 
 
 
 
 
 
+
+
+
+}
+
+
+function grayscale(img) {
+    var greyscale_img;
     img.loadPixels();
+    greyscale_img = createImage(img.width, img.height);
 
+    // 复制图像的像素数据
+
+    greyscale_img.loadPixels();
+    for (let i = 0; i < img.pixels.length; i++) {
+        greyscale_img.pixels[i] = img.pixels[i];
+    }
+    greyscale_img.updatePixels();
+
+    greyscale_img.loadPixels();
+    for(var y=0;y<greyscale_img.height;y++){
+        for(var x=0;x<greyscale_img.width;x++){
+
+            var pixelIndex = ((greyscale_img.width * y) + x)*4;
+            var pixelRed = greyscale_img.pixels[pixelIndex + 0];
+            var pixelGreen = greyscale_img.pixels[pixelIndex + 1];
+            var pixelBlue = greyscale_img.pixels[pixelIndex + 2];
+            var pixelAlpha = greyscale_img.pixels[pixelIndex + 3];
+            var ave = (pixelRed + pixelGreen + pixelBlue)/3;
+            //set RGB to ave value
+
+            ave=min(ave * 1.2, 255);
+            greyscale_img.pixels[pixelIndex+0] = ave;
+            greyscale_img.pixels[pixelIndex+1] = ave;
+            greyscale_img.pixels[pixelIndex+2] = ave;
+        }
+    }
+    greyscale_img.updatePixels();
+    image(greyscale_img,img.width,0);
+}
+
+function RGB_channels(img) {
+    img.loadPixels();
 
     var redImg = createImage(img.width, img.height);
     redImg.loadPixels();
@@ -65,49 +107,7 @@ function setup() {
     blueImg.updatePixels();
 
 
-    image(img,0,0);
-
     image(redImg,0,redImg.height);
     image(greenImg,greenImg.width,greenImg.height);
     image(blueImg,blueImg.width*2,blueImg.height);
-
-
 }
-
-
-function grayscale(img) {
-    var greyscale_img;
-    img.loadPixels();
-    greyscale_img = createImage(img.width, img.height);
-
-    // 复制图像的像素数据
-
-    greyscale_img.loadPixels();
-    for (let i = 0; i < img.pixels.length; i++) {
-        greyscale_img.pixels[i] = img.pixels[i];
-    }
-    greyscale_img.updatePixels();
-
-    greyscale_img.loadPixels();
-    for(var y=0;y<greyscale_img.height;y++){
-        for(var x=0;x<greyscale_img.width;x++){
-
-            var pixelIndex = ((greyscale_img.width * y) + x)*4;
-            var pixelRed = greyscale_img.pixels[pixelIndex + 0];
-            var pixelGreen = greyscale_img.pixels[pixelIndex + 1];
-            var pixelBlue = greyscale_img.pixels[pixelIndex + 2];
-            var pixelAlpha = greyscale_img.pixels[pixelIndex + 3];
-            var ave = (pixelRed + pixelGreen + pixelBlue)/3;
-            //set RGB to ave value
-
-            ave=min(ave * 1.2, 255);
-            greyscale_img.pixels[pixelIndex+0] = ave;
-            greyscale_img.pixels[pixelIndex+1] = ave;
-            greyscale_img.pixels[pixelIndex+2] = ave;
-        }
-    }
-    greyscale_img.updatePixels();
-    image(greyscale_img,img.width,0);
-}
-
-
