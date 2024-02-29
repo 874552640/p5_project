83551e1c4dd19f46ea9ac4b6c63cfaf393137d0a
Chirs <874552640@qq.com>
2024-02-29 10:45:30 +0800

    脸部识别功能

--- sketch.js
+++ sketch.js
@@ -4,6 +4,10 @@ var redSlider;
 var greenSlider;
 var blueSlider;
 
+var faceImg;
+var detector;
+var classifier = objectdetect.frontalface;
+
 var imageLoaded = false;
 
 
@@ -14,11 +18,19 @@ function imageLoadedCallback(){
 
 function preload(){
     img = loadImage("assets/beansbag.jpeg",imageLoadedCallback);
+    faceImg = loadImage("assets/face1.jpg",imageLoadedCallback());
 
 }
 
 function setup() {
+    pixelDensity(1);
+    var scaleFactor = 1.2;
+
+    faceImg.resize(120,160);
+    detector = new objectdetect.detector(faceImg.width, faceImg.height, scaleFactor, classifier);
     createCanvas(1000,1000);
+
+
 
 
     redSlider = createSlider(0,256,125);
@@ -36,6 +48,8 @@ function setup() {
 
     RGB_channels(img);
 
+    image(img,0,img.height*3);
+    HSB_IMG(img);
 
 
 
@@ -130,6 +144,51 @@ function RGB_channels(img) {
     image(redImg,0,redImg.height);
     image(greenImg,greenImg.width,greenImg.height);
     image(blueImg,blueImg.width*2,blueImg.height);
+
+
+}
+
+function HSB_IMG(img){
+    var hsbImg = createImage(img.width, img.height);
+    hsbImg.loadPixels();
+
+    //load image pixel values into array pixels
+    img.loadPixels();
+    for(var y=0;y<img.height;y++){
+        for(var x=0;x<img.width;x++){
+
+            var pixelIndex = ((img.width * y) + x)*4;
+            var pixelRed = img.pixels[pixelIndex + 0];
+            var pixelGreen = img.pixels[pixelIndex + 1];
+            var pixelBlue = img.pixels[pixelIndex + 2];
+
+            var hsb = rgbToHsb(pixelRed,pixelGreen,pixelBlue);
+            //display h ueas red, saturation as green, brightness as  blue
+            hsbImg.pixels[pixelIndex+0] = hsb[0]*1.5;
+            hsbImg.pixels[pixelIndex+1] = hsb[1]*1.5;
+            hsbImg.pixels[pixelIndex+2] = hsb[2]*2;
+            hsbImg.pixels[pixelIndex+3] = 255;
+
+        }
+    }
+    hsbImg.updatePixels();
+
+    image(hsbImg,hsbImg.width,img.height*3);
+
+}
+
+function rgbToHsb(r, g, b) {
+    // Use the p5.js color() function to create a color object
+    let rgbColor = color(r, g, b);
+
+    //Extract HSB values from the color object
+    //Need to implement the algorithm for extracting hsv value from rgb for coursework.
+    let h = hue(rgbColor);
+    let s = saturation(rgbColor);
+    let br = brightness(rgbColor);
+
+    // Return the HSB values as an array
+    return [h, s, br];
 }
 
 function draw(){
@@ -190,4 +249,162 @@ function draw(){
     image(redImg,0,img.height*2);
     image(greenImg,blueImg.width,greenImg.height*2);
     image(blueImg,blueImg.width*2,blueImg.height*2);
+
+
+
+
+    //image(faceImg,0,0);
+    faces = detector.detect(faceImg.canvas);
+
+    strokeWeight(2);
+    stroke(0);
+    noFill();
+
+    faceImg.loadPixels();
+    for (var i=0; i<faces.length; i++){
+        var face=faces[i];
+        if (face[4] > 4){
+            // processPixels(int(face[0]), int(face[1]), int(face[2]), int(face[3]));
+            greyScale_face(int(face[0]), int(face[1]), int(face[2]), int(face[3]));
+            // blur_face(int(face[0]), int(face[1]), int(face[2]), int(face[3]));
+            pixel_face(int(face[0]), int(face[1]), int(face[2]), int(face[3]));
+        }
+    }
+
+    faceImg.updatePixels();
+    image(faceImg,0,faceImg.height*4);
+    noLoop();
+}
+
+function processPixels(startX,startY,dWidth,dHeight){
+
+    for(var y=startY;y<startY+dHeight;y++){
+        for(var x=startX;x<startX+dWidth;x++){
+
+            var pixelIndex = ((faceImg.width * y) + x)*4;
+            var pixelRed = faceImg.pixels[pixelIndex + 0];
+            var pixelGreen = faceImg.pixels[pixelIndex + 1];
+            var pixelBlue = faceImg.pixels[pixelIndex + 2];
+
+            //red channel only
+            faceImg.pixels[pixelIndex+0] = pixelRed;
+            faceImg.pixels[pixelIndex+1] = 0;
+            faceImg.pixels[pixelIndex+2] = 0;
+            faceImg.pixels[pixelIndex+3] = 255;
+
+        }
+    }
+}
+
+function greyScale_face(startX,startY,dWidth,dHeight){
+
+    for(var y=startY;y<startY+dHeight;y++){
+        for(var x=startX;x<startX+dWidth;x++){
+
+            var pixelIndex = ((faceImg.width * y) + x)*4;
+            var pixelRed = faceImg.pixels[pixelIndex + 0];
+            var pixelGreen = faceImg.pixels[pixelIndex + 1];
+            var pixelBlue = faceImg.pixels[pixelIndex + 2];
+
+            var ave = (pixelRed + pixelGreen + pixelBlue)/3;
+            //set RGB to ave value
+
+            ave=min(ave * 1.2, 255);
+            faceImg.pixels[pixelIndex+0] = ave;
+            faceImg.pixels[pixelIndex+1] = ave;
+            faceImg.pixels[pixelIndex+2] = ave;
+
+        }
+    }
+}
+
+function blur_face(startX,startY,dWidth,dHeight){
+
+    var matrix = getSimpleBlurKernel(20);
+
+    for(var x=startX;x<startX+dWidth;x++){
+        for(var y=startY;y<startY+dHeight;y++){
+            var pixelIndex = ((faceImg.width * y) + x)*4;
+            var r = faceImg.pixels[pixelIndex+0];
+            //calculate the convolution value for that pixel
+            var c = convolution(x,y,matrix,faceImg);
+            //update each pixel with new RGB value
+            faceImg.pixels[pixelIndex+0] = c[0];
+            faceImg.pixels[pixelIndex+1] = c[1];
+            faceImg.pixels[pixelIndex+2] = c[2];
+        }
+    }
+}
+
+//for simple averaging or blurring operation.
+function getSimpleBlurKernel(size){
+    var m = [];
+    for(var i=0;i<size;i++){
+        var n = [];
+        for(var j=0;j<size;j++){
+            n.push(1/(size*size));
+        }
+        m.push(n);
+    }
+    return m;
+}
+
+function convolution(x, y, matrix, img) {
+    var matrixSize = matrix.length;
+    var totalRed = 0.0;
+    var totalGreen = 0.0;
+    var totalBlue = 0.0;
+    var offset = floor(matrixSize / 2);
+
+    // convolution matrix loop
+    for (var i = 0; i < matrixSize; i++) {
+        for (var j = 0; j < matrixSize; j++) {
+            // Get pixel loc within convolution matrix
+            var xloc = x + i - offset;
+            var yloc = y + j - offset;
+            var index = (xloc + img.width * yloc) * 4;
+            // ensure we don't address a pixel that doesn't exist
+            index = constrain(index, 0, img.pixels.length - 1);
+
+            // multiply all values with the mask and sum up
+            totalRed += img.pixels[index + 0] * matrix[i][j];
+            totalGreen += img.pixels[index + 1] * matrix[i][j];
+            totalBlue += img.pixels[index + 2] * matrix[i][j];
+        }
+    }
+    // return the new color as an array
+    return [totalRed, totalGreen, totalBlue];
+}
+
+function pixel_face(startX,startY,dWidth,dHeight){
+
+    var pixel_block=5;
+    for(var y=startY;y<startY+dHeight;y+=pixel_block){
+        for(var x=startX;x<startX+dWidth;x+=pixel_block){
+
+            var sum=0;
+            var count=0;
+            for(var y_1=y;y_1<y+pixel_block;y_1++) {
+                for (var x_1 = x; x_1 < x + pixel_block; x_1++) {
+                    count++;
+                    // sum+=(faceImg.get(x_1,y_1))[0];
+                    var pixelIndex = ((faceImg.width * y_1) + x_1)*4;
+                    sum+=faceImg.pixels[pixelIndex+0];
+
+                }
+            }
+            var average=sum/count;
+
+            for(var y_2=y;y_2<y+pixel_block;y_2++) {
+                for (var x_2 = x; x_2 < x + pixel_block; x_2++) {
+                    var pixelIndex = ((faceImg.width * y_2) + x_2)*4;
+                    faceImg.pixels[pixelIndex+0] = average;
+                    faceImg.pixels[pixelIndex+1] = average;
+                    faceImg.pixels[pixelIndex+2] = average;
+
+                }
+            }
+
+        }
+    }
 }
